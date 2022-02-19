ae288ca64da5677c11224326d3b0f9b3e20c833e
Selier <Selier@163.com>
2022-02-19 22:17:55 +0800

    月季年报、监理日记/志添加

---
+++ ant-design-vue-jeecg/src/views/oa/supervision/modules/SupervisionDiaryModal.vue
@@ -0,0 +1,60 @@
+<template>
+  <j-modal
+    :title="title"
+    :width="width"
+    :visible="visible"
+    switchFullscreen
+    @ok="handleOk"
+    :okButtonProps="{ class:{'jee-hidden': disableSubmit} }"
+    @cancel="handleCancel"
+    cancelText="关闭">
+    <supervision-diary-form ref="realForm" @ok="submitCallback" :disabled="disableSubmit"></supervision-diary-form>
+  </j-modal>
+</template>
+
+<script>
+
+  import SupervisionDiaryForm from './SupervisionDiaryForm'
+  export default {
+    name: 'SupervisionDiaryModal',
+    components: {
+      SupervisionDiaryForm
+    },
+    data () {
+      return {
+        title:'',
+        width:800,
+        visible: false,
+        disableSubmit: false
+      }
+    },
+    methods: {
+      add () {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.add();
+        })
+      },
+      edit (record) {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.edit(record);
+        })
+      },
+      close () {
+        this.$emit('close');
+        this.visible = false;
+      },
+      handleOk () {
+        this.$refs.realForm.submitForm();
+      },
+      submitCallback(){
+        this.$emit('ok');
+        this.visible = false;
+      },
+      handleCancel () {
+        this.close()
+      }
+    }
+  }
+</script>
---
+++ ant-design-vue-jeecg/src/views/oa/supervision/modules/SupervisionDiaryModal1.vue
@@ -0,0 +1,60 @@
+<template>
+  <j-modal
+    :title="title"
+    :width="width"
+    :visible="visible"
+    switchFullscreen
+    @ok="handleOk"
+    :okButtonProps="{ class:{'jee-hidden': disableSubmit} }"
+    @cancel="handleCancel"
+    cancelText="关闭">
+    <supervision-diary-form1 ref="realForm1" @ok="submitCallback" :disabled="disableSubmit"></supervision-diary-form1>
+  </j-modal>
+</template>
+
+<script>
+
+  import SupervisionDiaryForm1 from './SupervisionDiaryForm1'
+  export default {
+    name: 'SupervisionDiaryModal1',
+    components: {
+      SupervisionDiaryForm1
+    },
+    data () {
+      return {
+        title:'',
+        width:800,
+        visible: false,
+        disableSubmit: false
+      }
+    },
+    methods: {
+      add () {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.add();
+        })
+      },
+      edit (record) {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.edit(record);
+        })
+      },
+      close () {
+        this.$emit('close');
+        this.visible = false;
+      },
+      handleOk () {
+        this.$refs.realForm.submitForm();
+      },
+      submitCallback(){
+        this.$emit('ok');
+        this.visible = false;
+      },
+      handleCancel () {
+        this.close()
+      }
+    }
+  }
+</script>
