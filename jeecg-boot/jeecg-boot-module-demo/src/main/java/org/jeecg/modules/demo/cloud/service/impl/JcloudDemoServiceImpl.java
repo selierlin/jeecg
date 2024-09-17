aea3fe8df15001ef4e5a450cb2bf06850431ba36
Selier <selier@163.com>
2024-09-17 16:34:34 +0800

    JcloudDemoServiceImpl.java

--- jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo/cloud/service/impl/JcloudDemoServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo/cloud/service/impl/JcloudDemoServiceImpl.java
@@ -1,13 +1,13 @@
-package org.jeecg.modules.demo.cloud.service.impl;
-
-import org.jeecg.common.api.vo.Result;
-import org.jeecg.modules.demo.cloud.service.JcloudDemoService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class JcloudDemoServiceImpl implements JcloudDemoService {
-    @Override
-    public Result<String> getMessage(String name) {
-        return Result.OK("Hello，" + name);
-    }
-}
+// package org.jeecg.modules.demo.cloud.service.impl;
+//
+// import org.jeecg.common.api.vo.Result;
+// import org.jeecg.modules.demo.cloud.service.JcloudDemoService;
+// import org.springframework.stereotype.Service;
+//
+// @Service
+// public class JcloudDemoServiceImpl implements JcloudDemoService {
+//     @Override
+//     public Result<String> getMessage(String name) {
+//         return Result.OK("Hello，" + name);
+//     }
+// }
